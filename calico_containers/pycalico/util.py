be9e504ef884ad80ff167ca6a10910e5caf97218
Luke Mino-Altherr <luke.mino-altherr@metaswitch.com>
2015-11-17 12:42:54 -0800

    Validate Rule init arguments

--- calico_containers/pycalico/util.py
+++ calico_containers/pycalico/util.py
@@ -91,3 +91,62 @@ def get_hostname():
         # The user does not have a set $HOSTNAME. Since this is a common
         # scenario, return socekt.gethostname instead of just erroring.
         return socket.gethostname()
+
+def validate_ports(port_list):
+    """
+    Checks whether a list of ports are within range of 0 and 65535.
+    The port list must include a number or a number range.
+
+    A valid number range must be two numbers delimited by a colon with the
+    second number higher than the first. Both numbers must be within range.
+    If a number range is invalid, the function will return False.
+
+    :param port_list:
+    :return: a Boolean: True if in range, False if not in range
+    """
+    in_range = True
+    for port in port_list:
+        if ":" in str(port):
+            ports = port.split(":")
+            in_range = (len(ports) == 2) and (int(ports[0]) < int(ports[1])) \
+                       and validate_ports(ports)
+        else:
+            try:
+                in_range = 0 <= int(port) < 65536
+            except ValueError:
+                in_range = False
+        if not in_range:
+            break
+
+    return in_range
+
+def validate_characters(input_string):
+    """
+    Validate that characters in string are supported by Felix.
+    Felix supports letters a-z, numbers 0-9, and symbols _.-
+
+    :param input_string: string to be validated
+    :return: Boolean: True if valid, False if invalid
+    """
+    # List of valid characters that Felix permits
+    valid_chars = '[a-zA-Z0-9_\.\-]'
+
+    # Check for invalid characters
+    if not re.match("^%s+$" % valid_chars, input_string):
+        return False
+    else:
+        return True
+
+def validate_icmp_type(icmp_type):
+    """
+    Validate that icmp_type is an integer between 0 and 255.
+    If not return False.
+
+    :param icmp_type:
+    :return: Boolean: True if valid icmp type, False if not
+    """
+    try:
+        valid = 0 <= int(icmp_type) < 255
+    except ValueError:
+        valid = False
+    return valid
