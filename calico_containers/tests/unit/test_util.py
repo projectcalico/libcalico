1564e8d021b49b6cee980fddfe5ebce882f729a6
Neil Jerram <Neil.Jerram@metaswitch.com>
2016-05-12 13:10:45 +0100

    Clean up trailing whitespace

--- calico_containers/tests/unit/test_util.py
+++ calico_containers/tests/unit/test_util.py
@@ -22,27 +22,27 @@ from nose_parameterized import parameterized
 
 MOCK_IP_ADDR = \
 """
-1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default 
+1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default
     link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
     inet 127.0.0.1/8 scope host lo
        valid_lft forever preferred_lft forever
-    inet6 ::1/128 scope host 
+    inet6 ::1/128 scope host
        valid_lft forever preferred_lft forever
 2: eth0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UP group default qlen 1000
     link/ether 08:00:27:73:c8:d0 brd ff:ff:ff:ff:ff:ff
     inet 172.24.114.18/24 brd 172.24.114.255 scope global eth0
        valid_lft forever preferred_lft forever
-    inet6 2620:104:4008:69:8d7c:499f:2f04:9e55/64 scope global temporary dynamic 
+    inet6 2620:104:4008:69:8d7c:499f:2f04:9e55/64 scope global temporary dynamic
        valid_lft 603690sec preferred_lft 84690sec
-    inet6 2620:104:4008:69:a00:27ff:fe73:c8d0/64 scope global dynamic 
+    inet6 2620:104:4008:69:a00:27ff:fe73:c8d0/64 scope global dynamic
        valid_lft 604698sec preferred_lft 86298sec
-    inet6 fe80::a00:27ff:fe73:c8d0/64 scope link 
+    inet6 fe80::a00:27ff:fe73:c8d0/64 scope link
        valid_lft forever preferred_lft forever
-3: docker0: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 qdisc noqueue state DOWN group default 
+3: docker0: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 qdisc noqueue state DOWN group default
     link/ether 00:00:00:00:00:00 brd ff:ff:ff:ff:ff:ff
     inet 172.17.42.1/24 scope global docker0
        valid_lft forever preferred_lft forever
-    inet6 fe80::188f:d6ff:fe1f:1482/64 scope link 
+    inet6 fe80::188f:d6ff:fe1f:1482/64 scope link
        valid_lft forever preferred_lft forever
 """
 
@@ -74,8 +74,8 @@ MOCK_IP_ADDR_DOCKER_NONE = \
 
 MOCK_IP_ADDR_LOOPBACK = \
 """
-1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 
-    inet6 ::1/128 scope host 
+1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536
+    inet6 ::1/128 scope host
        valid_lft forever preferred_lft forever
     inet 127.0.0.1/8 scope host lo
        valid_lft forever preferred_lft forever
